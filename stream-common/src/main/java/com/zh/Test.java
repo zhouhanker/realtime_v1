6cd7f251aa71dbd103d28cd275090a80c7769d80
zhouhan <zhouhanker@gmail.com>
2024-12-14 11:51:49 +0800

    add Test Demo

---
+++ stream-common/src/main/java/com/zh/Test.java
@@ -0,0 +1,13 @@
+package com.zh;
+
+/**
+ * @Package com.zh.Test
+ * @Author zhou.han
+ * @Date 2024/12/14 11:50
+ * @description: Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("yes !");
+    }
+}
